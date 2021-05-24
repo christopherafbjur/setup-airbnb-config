e4266f07bc4aa80730c9c736d8ea9bf820f6420f
Christopher Af Bjur <chrisdevcabc@gmail.com>
2021-05-24 15:11:41 +0200

    Create createProject function and basic logic for copying files and outputting status logs

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
 import arg from "arg";
 import inquirer from "inquirer";
+import { createProject } from "./main";
 
 function parseArgsIntoOptions(rawArgs) {
   const args = arg(
@@ -60,5 +61,5 @@ async function promptForMissingOptions(options) {
 export async function cli(args) {
   let options = parseArgsIntoOptions(args);
   options = await promptForMissingOptions(options);
-  console.log(options);
+  await createProject(options);
 }
---
+++ src/main.js
@@ -0,0 +1,42 @@
+import chalk from "chalk";
+import fs from "fs";
+import ncp from "ncp";
+import path from "path";
+import { promisify } from "util";
+
+const access = promisify(fs.access);
+const copy = promisify(ncp);
+
+async function copyTemplateFiles(options) {
+  return copy(options.templateDirectory, options.targetDirectory, {
+    clobber: false,
+  });
+}
+
+export async function createProject(options) {
+  options = {
+    ...options,
+    targetDirectory: options.targetDirectory || process.cwd(),
+  };
+
+  const currentFileUrl = import.meta.url;
+  const templateDir = path.resolve(
+    new URL(currentFileUrl).pathname,
+    "../../templates",
+    options.template.toLowerCase()
+  );
+  options.templateDirectory = templateDir;
+
+  try {
+    await access(templateDir, fs.constants.R_OK);
+  } catch (err) {
+    console.error("%s Invalid template name", chalk.red.bold("ERROR"));
+    process.exit(1);
+  }
+
+  console.log("Copy project files");
+  await copyTemplateFiles(options);
+
+  console.log("%s Project ready", chalk.green.bold("DONE"));
+  return true;
+}
