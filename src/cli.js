e8bc400aeb4deffa69c4ff8830f3cb1bcbdd1070
Christopher Af Bjur <chrisdevcabc@gmail.com>
2021-05-24 14:56:41 +0200

    Added support for parsing args and handling input

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,64 @@
-export function cli(args) {
-  console.log(args);
+import arg from "arg";
+import inquirer from "inquirer";
+
+function parseArgsIntoOptions(rawArgs) {
+  const args = arg(
+    {
+      "--test": Boolean,
+      "--yes": Boolean,
+      "-t": "--test",
+      "-y": "--yes",
+    },
+    {
+      argv: rawArgs.slice(2),
+    }
+  );
+  return {
+    skipPrompts: args["--yes"] || false,
+    test: args["--test"] || false,
+    template: args._[0],
+  };
+}
+
+async function promptForMissingOptions(options) {
+  const defaultTemplate = "JavaScript";
+  if (options.skipPrompts) {
+    return {
+      ...options,
+      template: options.template || defaultTemplate,
+    };
+  }
+
+  const questions = [];
+  if (!options.template) {
+    questions.push({
+      type: "list",
+      name: "template",
+      message: "Please choose which project template to use",
+      choices: ["JavaScript", "React"],
+      default: defaultTemplate,
+    });
+  }
+
+  if (!options.git) {
+    questions.push({
+      type: "confirm",
+      name: "test",
+      message: "This is a test",
+      default: false,
+    });
+  }
+
+  const answers = await inquirer.prompt(questions);
+  return {
+    ...options,
+    template: options.template || answers.template,
+    test: options.test || answers.test,
+  };
+}
+
+export async function cli(args) {
+  let options = parseArgsIntoOptions(args);
+  options = await promptForMissingOptions(options);
+  console.log(options);
 }
