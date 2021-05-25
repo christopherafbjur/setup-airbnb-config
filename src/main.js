18189cf6aae313e9df3288dda9ef96e0ef4fdd44
Christopher Af Bjur <chrisdevcabc@gmail.com>
2021-05-25 18:14:15 +0200

    Added dynamic build functions for package.json and eslint/prettier configuration files fixes #3

--- src/main.js
+++ src/main.js
@@ -43,6 +43,10 @@ export async function createProject(options) {
       task: () => copyTemplateFiles(options),
     },
     {
+      title: "Configure user settings",
+      task: () => buildDynamicFiles(options),
+    },
+    {
       title: "Initialize git",
       task: () => initializeGit(options),
       enabled: () => options.git,
@@ -106,4 +110,67 @@ async function installEslintConfigReact(options) {
     return Promise.reject(new Error("Failed to install eslint config"));
   }
   return;
+}
+
+function buildDynamicFiles(options) {
+  //TODO Add support for different data config depending on options.template
+  buildPackageJson(options);
+  buildEslintRcJson(options);
+  buildPrettierRcJson(options);
+}
+
+function buildPackageJson(options) {
+  const fullPath = path.resolve(options.targetDirectory, "package.json");
+  const data = {
+    name: "create-project-javascript-template",
+    version: "1.0.0",
+    description: "",
+    main: "index.js",
+    scripts: {
+      test: 'echo "Error: no test specified" && exit 1',
+    },
+    keywords: [],
+    author: "Christopher Af Bjur",
+    license: "ISC",
+    devDependencies: {
+      eslint: "^7.27.0",
+      "eslint-config-node": "^4.1.0",
+      "eslint-config-prettier": "^8.3.0",
+      "eslint-plugin-node": "^11.1.0",
+      "eslint-plugin-prettier": "^3.4.0",
+      prettier: "^2.3.0",
+    },
+  };
+
+  saveDataFile(fullPath, data);
+}
+
+function buildEslintRcJson(options) {
+  const fullPath = path.resolve(options.targetDirectory, ".eslintrc.json");
+  const data = {
+    extends: ["airbnb-base", "prettier", "plugin:node/recommended"],
+    plugins: ["prettier"],
+    rules: {
+      "prettier/prettier": "error",
+      "no-unused-vars": "warn",
+      "no-console": "off",
+      "func-names": "off",
+      "no-process-exit": "off",
+      "object-shorthand": "off",
+      "class-methods-use-this": "off",
+    },
+  };
+
+  saveDataFile(fullPath, data);
+}
+
+function buildPrettierRcJson(options) {
+  const fullPath = path.resolve(options.targetDirectory, ".prettierrc.json");
+  const data = {};
+
+  saveDataFile(fullPath, data);
+}
+
+function saveDataFile(filePath, data) {
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
 }
