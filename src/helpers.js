53d8fb7336786f9671f744eed5b1e6762b131bc9
Christopher Af Bjur <chrisdevcabc@gmail.com>
2021-06-02 19:26:07 +0200

    Added manager map to getPackageManager function

--- src/helpers.js
+++ src/helpers.js
@@ -1,16 +1,19 @@
 import fs from "fs";
+import path from "path";
 
 export function getPackageManager(options) {
-  if (options.useYarn)
-    return {
+  const managers = {
+    yarn: {
       name: "yarn",
       commands: ["add", "--dev"],
-    };
-
-  return {
-    name: "npm",
-    commands: ["i", "-D"],
+    },
+    npm: {
+      name: "npm",
+      commands: ["i", "-D"],
+    },
   };
+
+  return managers[options.manager];
 }
 
 export function saveDataFile(filePath, data) {
@@ -19,4 +22,35 @@ export function saveDataFile(filePath, data) {
 
 export function readDataFile(filePath) {
   return JSON.parse(fs.readFileSync(filePath));
+}
+
+export function detectPackageManager(options, callback) {
+  const dir = options.targetDirectory;
+  const yarn = fileExists(path.resolve(dir, "yarn.lock"));
+  const npm = fileExists(path.resolve(dir, "package-lock.json"));
+
+  if (yarn && npm)
+    return callback({
+      detected: null,
+      error:
+        "Multiple package managers detected for project. Which package manager be used for installing dependencies to this project?",
+    });
+
+  if (yarn) return callback({ detected: "yarn", error: null });
+  if (npm) return callback({ detected: "npm", error: null });
+
+  return callback({
+    detected: null,
+    error:
+      "Unable to detect package manager to for project. Which package manager be used for installing dependencies to this project?",
+  });
+}
+
+function fileExists(filePath) {
+  try {
+    if (fs.existsSync(filePath)) return true;
+    return false;
+  } catch (err) {
+    return false;
+  }
 }
