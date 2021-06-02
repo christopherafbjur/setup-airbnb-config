849e5f2fab21ef8b4ccd949332c84f130c8674c5
Christopher Af Bjur <chrisdevcabc@gmail.com>
2021-06-02 14:22:05 +0200

    Install only neccesary deps based on template

--- src/tasks/installDependencies.js
+++ src/tasks/installDependencies.js
@@ -1,14 +1,18 @@
 import execa from "execa";
 
 export default function (options) {
+  const template = options.template.toLowerCase();
   const dependencies = [
     "eslint@^7.27.0",
     "prettier@^2.3.0",
     "eslint-plugin-prettier@^3.4.0",
     "eslint-config-prettier@^8.3.0",
-    "eslint-plugin-node@^11.1.0",
-    "eslint-config-node@^4.1.0",
   ];
+  const jsDeps = ["eslint-plugin-node@^11.1.0", "eslint-config-node@^4.1.0"];
+
+  if (template === "javascript") {
+    jsDeps.forEach((jsDep) => dependencies.push(jsDep));
+  }
 
   return {
     title: "Install eslint/prettier deps",
