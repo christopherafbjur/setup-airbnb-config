905e8d94ed1a4d3f814757e5b7b25a0cbaa157ac
Christopher Af Bjur <chrisdevcabc@gmail.com>
2021-06-02 14:21:24 +0200

    Solve bug caused by running "npx install-peerdeps --dev eslint-config-airbnb". Instead list peerdeps and install manually using npm. Use npm install for JS as well for better code structure

--- src/tasks/installPeerDependencies.js
+++ src/tasks/installPeerDependencies.js
@@ -1,8 +1,17 @@
 import execa from "execa";
 
 function getTemplateDependency(template) {
-  if (template === "react") return "eslint-config-airbnb@^18.2.1";
-  return "eslint-config-airbnb-base@^14.2.1";
+  const dependencies = ["eslint@^7.2.0", "eslint-plugin-import@^2.22.1"];
+  const reactDeps = [
+    "eslint-plugin-jsx-a11y@^6.4.1",
+    "eslint-plugin-react@^7.21.5",
+    "eslint-plugin-react-hooks@^4",
+  ];
+
+  if (template === "react") {
+    reactDeps.forEach((reactDep) => dependencies.push(reactDep));
+  }
+  return dependencies;
 }
 export default function (options) {
   const template = options.template.toLowerCase();
@@ -11,8 +20,8 @@ export default function (options) {
     title: `Install Peer Dependencies (${template})`,
     task: async () => {
       const result = await execa(
-        "npx",
-        ["install-peerdeps", "--dev", getTemplateDependency(template)],
+        "npm",
+        ["i", "-D", getTemplateDependency(template)],
         {
           cwd: options.targetDirectory,
         }
