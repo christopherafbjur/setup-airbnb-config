8d47da63173c7d109395abfbd008a1c6b7af0707
Christopher Af Bjur <chrisdevcabc@gmail.com>
2021-06-03 23:04:20 +0200

    Added manual/help arg

--- src/cli.js
+++ src/cli.js
@@ -1,42 +1,45 @@
-import arg from "arg";
-import chalk from "chalk";
-import inquirer from "inquirer";
-import { createProject } from "./main";
-import { detectPackageManager } from "./utils/helpers";
-import { packageJsonExists } from "./utils/helpers";
+import arg from 'arg';
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+import { createProject } from './main';
+import { detectPackageManager } from './utils/helpers';
+import { packageJsonExists } from './utils/helpers';
 
 function parseArgsIntoOptions(rawArgs, options) {
   let args;
   try {
     args = arg(
       {
-        "--yarn": Boolean,
-        "--npm": Boolean,
-        "--react": Boolean,
-        "--javascript": Boolean,
-        "-j": "--javascript",
-        "-r": "--react",
+        '--yarn': Boolean,
+        '--npm': Boolean,
+        '--react': Boolean,
+        '--javascript': Boolean,
+        '--help': Boolean,
+        '-j': '--javascript',
+        '-r': '--react',
+        '-h': '--help',
       },
       {
         argv: rawArgs.slice(2),
       }
     );
   } catch (err) {
-    console.error("%s ".concat(err.message), chalk.red.bold("ERROR "));
+    console.error('%s '.concat(err.message), chalk.red.bold('ERROR '));
     process.exit(1);
   }
 
   return {
     ...options,
+    displayHelp: args['--help'] || false,
     template: (function () {
-      if (args["--react"]) return "react";
-      if (args["--javascript"]) return "javascript";
+      if (args['--react']) return 'react';
+      if (args['--javascript']) return 'javascript';
 
       return null;
     })(),
     manager: (function () {
-      if (args["--npm"]) return "npm";
-      if (args["--yarn"]) return "yarn";
+      if (args['--npm']) return 'npm';
+      if (args['--yarn']) return 'yarn';
 
       return null;
     })(),
@@ -44,14 +47,15 @@ function parseArgsIntoOptions(rawArgs, options) {
 }
 
 function createOptions() {
+  console.log(process.cwd());
   return {
     targetDirectory: process.cwd(),
   };
 }
 
 async function promptForMissingOptions(options) {
-  const defaultTemplate = "javascript";
-  const defaultManager = "npm";
+  const defaultTemplate = 'javascript';
+  const defaultManager = 'npm';
 
   const questions = [];
 
@@ -61,9 +65,9 @@ async function promptForMissingOptions(options) {
         options.manager = detected;
       } else {
         questions.push({
-          type: "list",
-          name: "manager",
-          choices: ["npm", "yarn"],
+          type: 'list',
+          name: 'manager',
+          choices: ['npm', 'yarn'],
           message: error,
           default: defaultManager,
         });
@@ -73,12 +77,12 @@ async function promptForMissingOptions(options) {
 
   if (!options.template) {
     questions.push({
-      type: "list",
-      name: "template",
-      message: "Please choose what type of project this is",
+      type: 'list',
+      name: 'template',
+      message: 'Please choose what type of project this is',
       choices: [
-        { name: "JavaScript", value: "javascript" },
-        { name: "React", value: "react" },
+        { name: 'JavaScript', value: 'javascript' },
+        { name: 'React', value: 'react' },
       ],
       default: defaultTemplate,
     });
@@ -95,12 +99,30 @@ async function promptForMissingOptions(options) {
 export async function cli(args) {
   let options = createOptions();
 
-  if (!packageJsonExists(options)) {
-    console.error("%s Missing package.json.", chalk.red.bold("ERROR "));
+  options = parseArgsIntoOptions(args, options);
+
+  if (options.displayHelp) {
+    const help = `
+      Options
+        -j, --javascript    Generates a JS configuration
+        -r, --react         Generates a React configuration
+        --yarn              Enforce npm to be used when installing dependencies
+        --npm               Enforce yarn to be used when installing dependencies
+
+      Examples
+        $ setup-airbnb-config --r --yarn
+        $ setup-airbnb-config --javascript --npm
+        $ setup-airbnb-config
+`;
+    console.log(help);
     process.exit(1);
   }
 
-  options = parseArgsIntoOptions(args, options);
+  if (!packageJsonExists(options)) {
+    console.error('%s Missing package.json.', chalk.red.bold('ERROR '));
+    process.exit(1);
+  }
+
   options = await promptForMissingOptions(options);
 
   await createProject(options);
